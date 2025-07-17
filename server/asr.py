47cfc7a0071acfb130a3b051b2848158129dfdfc
devclassic <devclassic@yeah.net>
2025-07-17 11:30:51 +0800

    修改模型为本地运行模型

--- server/asr.py
+++ server/asr.py
@@ -2,8 +2,8 @@ from funasr import AutoModel
 
 model = AutoModel(
     disable_update=True,
-    model="paraformer-zh",
-    vad_model="fsmn-vad",
-    punc_model="ct-punc",
-    spk_model="cam++",
+    model="./models/paraformer-zh/iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    vad_model="./models/fsmn-vad/iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+    punc_model="./models/ct-punc/iic/punc_ct-transformer_cn-en-common-vocab471067-large",
+    spk_model="./models/cam++/iic/speech_campplus_sv_zh-cn_16k-common",
 )
---
+++ server/dlmodels.py
@@ -0,0 +1,13 @@
+from modelscope.hub.snapshot_download import snapshot_download
+
+# 模型列表及对应的ModelScope仓库地址
+model_repos = {
+    "paraformer-zh": "iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    "fsmn-vad": "iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+    "ct-punc": "iic/punc_ct-transformer_cn-en-common-vocab471067-large",
+    "cam++": "iic/cccccspeech_campplus_sv_zh-cn_16k-common",
+}
+
+# 下载所有模型到本地目录（如./local_models）
+for model_name, repo_id in model_repos.items():
+    snapshot_download(repo_id, cache_dir=f"./models/{model_name}")
