87d80cb422a59672e5c0e1f9cb8e5432427e8c92
devclassic <devclassic@yeah.net>
2025-08-20 10:24:55 +0800

    修改参数

--- server/asr.py
+++ server/asr.py
@@ -1,10 +1,11 @@
 from funasr import AutoModel
 
 model = AutoModel(
-    disable_update=True,
     model="./models/funasr/paraformer-zh/iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
     vad_model="./models/funasr/fsmn-vad/iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
     punc_model="./models/funasr/ct-punc/iic/punc_ct-transformer_cn-en-common-vocab471067-large",
     spk_model="./models/funasr/cam++/iic/speech_campplus_sv_zh-cn_16k-common",
+    disable_update=True,
+    trust_remote_code=True,
     hotword="./hotwords.txt",
 )
