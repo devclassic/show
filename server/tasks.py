fa9bf3f6831338746a1ee2dcbbf4a2625f94582a
devclassic <devclassic@yeah.net>
2025-09-03 09:55:22 +0800

    修改打包逻辑

--- server/tasks.py
+++ server/tasks.py
@@ -3,4 +3,5 @@ from invoke import task
 
 @task
 def build(ctx):
-    ctx.run("pyinstaller -F --noupx --collect-all funasr --collect-all transformers main.py")
+    cmd = "pyinstaller -F --clean --noupx --collect-all funasr --collect-all transformers main.py"
+    ctx.run(cmd)
